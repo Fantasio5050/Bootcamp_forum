44218285c871d5c73de7bf45a2ca82ef503b7137
Joris <118273449+joris-lchpt@users.noreply.github.com>
2024-11-08 13:17:06 +0100

    Index MVC

--- index.php
+++ index.php
@@ -1,1 +1,38 @@
 <?php
+header('Cache-Control: no-cache, must-revalidate');
+header('Expires: Sat, 26 Jul 1997 05:00:00 GMT');
+header('Pragma: no-cache');
+
+$act;
+
+if (isset($_GET['action'])) {
+  $act = $_GET['action'] .".php";
+}
+else{
+  $act = 'Home.php';
+}
+
+//HEADER////////////////////
+include 'app/Controllers/header.php';
+include $viewH;
+////////////////////////////
+
+
+//CONATINER/////////////////
+if (file_exists('app/Controllers/' .$act)) {
+  include 'app/Controllers/' .$act;
+  include $view;
+}
+else{
+  include 'app/Controllers/Error.php';
+  include $viewE;
+}
+///////////////////////////
+
+//FOOTER///////////////////
+include 'App/Controllers/Footer.php';
+include $viewF;
+///////////////////////////
+
+
+ ?>
